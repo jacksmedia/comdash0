cc637f406a96f12b3279e762dcfb92aa9f35a510
jacksmedia <alex@j4cks.com>
2022-08-09 22:37:11 -0700

    :art::construction: adds assets

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ComDash - The Web3 Swiss-army Dashboard on The Elrond Blockchain.
         </p>
         <a
           className="App-link"
