25ed0d587e9a7388dddee771292665dc6514306f
jacksmedia <alex@j4cks.com>
2022-08-15 00:57:06 -0700

    :art: newer logo prototype + Bootstrap 5 CDN

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo1.png';
+import logo from './Comdash-Logo-White.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header container-sm p-3">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ComDash - The Web3 Swiss-army Dashboard on The Elrond Blockchain.
+          ComDash - The Web3 Swiss-army Dashboard on The Elrond Blockchain
         </p>
         <a
           className="App-link"
